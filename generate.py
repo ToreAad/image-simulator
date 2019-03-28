65c4836cef4554458a4de5b20af08aa613b120ea
ToreAad <tore.aad@gmail.com>
2019-03-28 14:35:26 +0100

    Make mkimage also save masks for image segmentation

--- generate.py
+++ generate.py
@@ -4,6 +4,9 @@ import random
 import glob
 import os
 import sys
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 # check if python3 is used
 if sys.version_info < (3, 0):
@@ -30,13 +33,15 @@ def initialize(backgrounds_dir, classes_dir):
 
     return objs, class_names, bgs
 
-# Simulate an image
-
 
 def mkimage(filename, objs, names, bgs, maxobjs, output_dir="images_out", single=False):
+    """
+    Simulate an image
+    """
     log = []
     im = bgs[random.randint(0, len(bgs)-1)].copy()
-    # print('bg size='+str(im.size))
+    seg_ims = [Image.fromarray(np.zeros(
+        (im.height, im.width), dtype=np.uint8)).convert("L") for _ in range(len(objs))]
     cls0 = random.randint(0, len(objs)-1)
     for _ in range(0, random.randint(1, maxobjs)):
         if single:
@@ -49,18 +54,28 @@ def mkimage(filename, objs, names, bgs, maxobjs, output_dir="images_out", single
         posx = random.randint(-floor(sizex/2), imx-floor(sizex/2))
         posy = random.randint(-floor(sizey/2), imy-floor(sizey/2))
         im.paste(obj, (posx, posy), obj)
+
+        obj_array = np.array(obj)
+        obj_mask = (obj_array[:, :, 3] != 0)
+        obj_mask = np.uint8(obj_mask)*255
+        obj_seg = Image.fromarray(obj_mask).convert("L")
+        seg_ims[cls].paste(obj_seg, (posx, posy), obj)
+
         log = log + \
             ['{}\t{}\t{}\t{}\t{}\t{}\n'.format(
                 names[cls], cls, posy, posx, posy+sizey, posx+sizex)]
     im.save(os.path.join(output_dir, filename+'.png'))
-    with open(os.path.join(output_dir, filename+'.txt'), 'w') as f:
-        for l in log:
-            f.write(l)
+    for i, seg_im in enumerate(seg_ims):
+        seg_im.save(os.path.join(output_dir, f'{filename}_segment{i}.png'))
 
-# Testing
+    with open(os.path.join(output_dir, filename+'.txt'), 'w') as f:
+        [f.write(l) for l in log]
 
 
 def test():
+    """
+    Testing
+    """
     objects, names, backgrounds = initialize(
         backgrounds_dir="backgrounds", classes_dir="crops")
     mkimage('test', objects, names, backgrounds,
